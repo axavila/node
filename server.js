9375634ad30a50ba88791fbcb4882fdbd665015d
axavila <ax.avila@hotmail.com>
2020-07-21 21:53:59 -0600

    up2

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 
 const app = express();
 
+
+app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.json());
 app.get('/', (req, res) => {
-    const user = {
-        name: 'sally',
-        hobby: 'soccer'
-    }
-    res.send(user);
+    res.send("getting root");
 });
+
+app.get('/profile', (req, res) => {
+    res.send("getting profile");
+});
+
+app.post('/profile', (req, res) => {
+    console.log(req.body)
+    res.send('Success');
+});
+
 
 app.listen(3000)
